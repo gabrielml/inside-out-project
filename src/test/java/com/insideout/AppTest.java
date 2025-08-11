03d24a4353910e9e7de59e90b9c3ea9a45247d4b
Gabriel ML <gabrielmljob@gmail.com>
2025-08-11 15:58:42 +0200

    chore: Disable 'AppTest' method.
    Temporarily disables the 'testApp' method using the '@Disabled'
    annotation. This test is being disabled because it is not relevant at
    this time.
    
    The test will be re-enabled or removed in a future commit once the
    associated functionality is either implemented or no longer needed.

--- src/test/java/com/insideout/AppTest.java
+++ src/test/java/com/insideout/AppTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Disabled;
+
 /**
  * Unit test for simple App.
  */
@@ -11,6 +13,8 @@ class AppTest {
     /**
      * Rigorous Test.
      */
+
+    @Disabled
     @Test
     void testApp() {
         assertEquals(1, 1);
